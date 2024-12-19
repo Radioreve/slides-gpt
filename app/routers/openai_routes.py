d3ea0afd464dfbdb4a2b724c384be1b0427f0557
LJA <leo.jacquemin@octo.com>
2024-12-19 21:03:27 +0100

    revue avec fifi

--- app/routers/openai_routes.py
+++ app/routers/openai_routes.py
@@ -5,11 +5,11 @@ from app.services.openai_service import generate_text_with_ai
 
 router = APIRouter()
 
+
 @router.post("/generate-text")
-async def generate_text(request: TextRequest):
+async def generate_text(request: TextRequest) -> TextSummary:
     try:
         summary = generate_text_with_ai(request.text)
         return TextSummary(summary=summary)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erreur lors de la génération du résumé : {e}")
-
+        raise HTTPException(status_code=500, detail=f"Erreur lors de la génération de texte par GenAI : {e}")
--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,21 +1,20 @@
 import openai
+
 from app.config import Config
 
 openai.api_key = Config.OPENAI_API_KEY
 
+
 def generate_text_with_ai(text: str) -> str:
     try:
         response = openai.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o",
             messages=[
-                {"role": "system", "content": "Vous êtes un assistant."},
-                {"role": "user", "content": f"{text}"}
+                {"role": "user", "content": text}
             ],
-            max_tokens=100
-
+            max_tokens=Config.MAX_TOKEN
         )
 
-        #return response["choices"][0]["message"]["content"].strip()
         return response.choices[0].message.content.strip()
     except Exception as e:
         raise Exception(f"Erreur lors de l'appel à l'API OpenAI : {e}")
